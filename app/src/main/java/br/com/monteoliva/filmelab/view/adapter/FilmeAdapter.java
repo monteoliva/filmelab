4fc53ac933016670b0382551216fddbc1e02a0ac
Claudio Monteoliva <claudio@monteoliva.net.br>
2018-12-06 19:22:25 -0200

    New version

--- app/src/main/java/br/com/monteoliva/filmelab/view/adapter/FilmeAdapter.java
+++ app/src/main/java/br/com/monteoliva/filmelab/view/adapter/FilmeAdapter.java
@@ -1,4 +1,55 @@
 package br.com.monteoliva.filmelab.view.adapter;
 
-public class FilmeAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import org.androidannotations.annotations.EBean;
+import org.androidannotations.annotations.RootContext;
+
+import java.util.List;
+
+import br.com.monteoliva.filmelab.model.FilmeDataBean;
+import br.com.monteoliva.filmelab.view.custom.ItemView;
+import br.com.monteoliva.filmelab.view.custom.ItemView_;
+
+@EBean
+public class FilmeAdapter extends BaseAdapter {
+    @RootContext
+    protected Context context;
+
+    private List<FilmeDataBean> list;
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public FilmeDataBean getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return list.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+
+        final FilmeDataBean item = getItem(position);
+
+        if (convertView == null) {
+            row = ItemView_.build(context);
+        }
+
+        return ((ItemView) row).bind(item);
+    }
+
+    public void setList(List<FilmeDataBean> list) {
+        this.list = list;
+    }
 }
--- app/src/main/java/br/com/monteoliva/filmelab/view/custom/ItemView.java
+++ app/src/main/java/br/com/monteoliva/filmelab/view/custom/ItemView.java
@@ -1,4 +1,54 @@
 package br.com.monteoliva.filmelab.view.custom;
 
-public class ItemView {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.androidannotations.annotations.EViewGroup;
+import org.androidannotations.annotations.ViewById;
+
+import br.com.monteoliva.filmelab.R;
+import br.com.monteoliva.filmelab.model.FilmeDataBean;
+
+@EViewGroup(R.layout.item_view)
+public class ItemView extends CardView {
+    @ViewById(R.id.imgFilm)
+    ImageView imageView;
+
+    @ViewById(R.id.titleFilm)
+    TextView title;
+
+    /**
+     * Constructor
+     *
+     * @param context
+     */
+    public ItemView(@NonNull Context context) { super(context); }
+    public ItemView(@NonNull Context context, @NonNull AttributeSet attrs) { super(context, attrs); }
+
+    /**
+     * Method to return Bind of this object
+     *
+     * @return
+     */
+    public ItemView bind(FilmeDataBean dataBean) {
+        // load image
+        Glide.with(getContext())
+                .load(dataBean.getPoster())
+                .into(imageView);
+
+        title.setText(dataBean.getTitle());
+
+
+
+
+        return this;
+    }
+
+
 }
